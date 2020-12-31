bad117922ed6ebfe819fe438339e213088c8a25e
Sangeetha Velamoor <sangeetha.sudhakar@gmail.com>
2020-12-31 16:32:16 -0500

    updated text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          {/* Hi there */}
+          Changes made in feature branch
         </p>
         <a
           className="App-link"
